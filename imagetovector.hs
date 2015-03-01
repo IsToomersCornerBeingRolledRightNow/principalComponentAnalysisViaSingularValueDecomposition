8a54251f9a832d1c724a1e1942ab7fa45b4b3655
Zachary Sarver <Zachary.Sarver@gmail.com>
2015-03-01 14:40:47 -0600

    Resizing

--- imagetovector.hs
+++ imagetovector.hs
@@ -3,8 +3,11 @@ import Data.Packed.Vector
 import Vision.Image
 --import Vision.Image.Storage.DevIL (Autodetect (..), load)
 
-imageToVector :: Manifest RGBPixel -> Vector Double
+imageToVector :: RGB -> Vector Double
 imageToVector = fromList . concatMap f . toList . manifestVector
   where
   f :: RGBPixel -> [Double]
   f (RGBPixel r g b) = [fromIntegral r, fromIntegral g, fromIntegral b]
+  
+reduceResolution :: Int -> Int -> RGB -> RGB
+reduceResolution h w img = resize NearestNeighbor (manifestSize img) img
