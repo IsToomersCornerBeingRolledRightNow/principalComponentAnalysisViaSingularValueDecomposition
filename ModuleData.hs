e1bc84d71d6f81eaf9df061fc1da12544b83fe35
Daniel Brice <danielbrice@gmail.com>
2015-03-03 08:18:40 -0600

    reverted back to previous state

--- ModuleData.hs
+++
@@ -1,9 +0,0 @@
-module ModuleData
-( Hyperplane
-) where
-
-import Data.Packed.Matrix
-import Data.Packed.Vector
-
-data Hyperplane = Hyperplane Int (Vector Double) (Matrix Double) deriving (Show, Read)
-
