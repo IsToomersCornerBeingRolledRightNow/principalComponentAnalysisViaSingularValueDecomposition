346ad9115c49a3f1ed7da9b4fa299aeb98bfd3df
Daniel Brice <danielbrice@gmail.com>
2015-03-05 10:08:52 -0600

    train.hs is for using training data to build the best-fit hyperplane.

--- train.hs
+++ train.hs
@@ -1,18 +1,24 @@
-{-|
+import PCA
+import ImageToVector
+
+import System.Directory
+import System.Environment
+import Data.Packed.Matrix
+import Data.Packed.Vector
+
 main :: IO ()
 main = do
-  [dir, height, width, threshold] <- getArgs
+  [dir, width, height, threshold] <- getArgs
   let h' = read height
   let w' = read width
   let t' = read threshold
   imageFiles <- getDirectoryContents dir
   let imagePaths = map (("./" ++ dir) ++) imageFiles
-  maybeImages <- loadImages imagePaths h' w'
+  maybeImages <- loadImages w' h' imagePaths
   let rows = [a | Just a <- maybeImages]
   let matrix = fromRows rows
-  let linreg = makePlane matrix t'
-  print linreg  
--}  
+  let linreg = linRegression t' matrix
+  print linreg
 
 
 {-|
