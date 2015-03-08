119b301ffe1ab922042ef7a789a94150048c38c2
Daniel Brice <danielbrice@gmail.com>
2015-03-08 11:36:54 -0500

    pushing this so alex can inspect

--- meanframe.hs
+++ meanframe.hs
@@ -6,12 +6,19 @@ import Data.Packed.Vector
 import Numeric.LinearAlgebra.HMatrix (scale)
 import Vision.Primitive.Shape (ix2)
 import Vision.Image.Storage.DevIL
+import Data.List (foldl')
 
 main = do
   dir:_ <- getArgs
   let f = loadImagesToVectors 1280 720 . fmap ((dir++)"/"++)
   imgs <- fmap catMaybes $ f =<< getDirectoryContents dir
-  let avg = scale (1 / (fromIntegral $ length imgs)) (sum imgs)
-  let img = vectorToImage (ix2 1280 720) avg
-  save PNG (dir ++ "/average/average.png") img
+  let avgimg = vectorToImage (ix2 1280 720) $ avg imgs
+  save PNG (dir ++ "/average/average.png") avgimg
+
+avg :: [Vector Double] -> Vector Double
+avg xs = scale (1 / fromIntegral num) sum
+  where
+  (num, sum) = foldl' f zero xs
+  zero = (0, fromList . take (1280*720*3) . repeat $ 0)
+  f (n,s) v = n `seq` s `seq` (n+1,v+s)
 
